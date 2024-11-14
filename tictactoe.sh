1ef488a0b3f0774a5ac41747f0ef16ca3c44b8cb
Saad Monem <smonem@umich.edu>
2024-11-13 21:32:50 -0500

    Added flags to script for resetting stats and AI player 1

--- tictactoe.sh
+++ tictactoe.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Script options
+player_1_human=true
+while getopts 'ra' flag; do
+  case "${flag}" in
+    r) # reset
+    rm "./tictactoe_stats.csv" ;;
+    a) # auto
+    player_1_human=false ;;
+    *) # Invalid
+    printf "Option not recognized\n"
+  esac
+done
+
 # Setup stats file
 stats="tictactoe_stats.csv"
 
@@ -9,7 +22,7 @@ if [ ! -f "$stats" ]; then
 fi
 
 # Init tic-tac-toe array and stats
-ttt=(0 0 0 0 0 0 0 0 0)
+board=(0 0 0 0 0 0 0 0 0)
 num_moves=0
 game_date=$(date '+%Y-%m-%d %H:%M:%S')
 
@@ -18,9 +31,9 @@ function print_grid() {
     printf "\n"
     for row in {0..2}; do
         for col in {0..2}; do
-            if [ "${ttt[row*3+col]}" == 0 ]; then
+            if [ "${board[row*3+col]}" == 0 ]; then
                 printf "* "
-            elif [ "${ttt[row*3+col]}" == 1 ]; then
+            elif [ "${board[row*3+col]}" == 1 ]; then
                 printf "X "
             else
                 printf "0 "
@@ -38,7 +51,7 @@ function game_end() {
     else
         echo -e "\nDRAW"
     fi
-    echo "$game_date, $1, $num_moves, ${ttt[*]}" >> $stats
+    echo "$game_date, $1, $num_moves, ${board[*]}" >> $stats
     exit 0
 }
 
@@ -59,8 +72,8 @@ function check_board_state() {
     # Rows and cols
     for ((i = 0 ; i < 3; i++));
         do for ((j = 0 ; j < 3 ; j++));
-            do ((target_row+=ttt[i*3+j]))
-            ((target_col+=ttt[j*3+i])) 
+            do ((target_row+=board[i*3+j]))
+            ((target_col+=board[j*3+i])) 
         done
         win_check $target_row
         win_check $target_col
@@ -69,48 +82,53 @@ function check_board_state() {
     done
 
     # Diagionals
-    ((target+=(ttt[0]+ttt[4]+ttt[8]))) 
+    ((target+=(board[0]+board[4]+board[8]))) 
     win_check $target
     target=0 # reset
 
-    ((target+=(ttt[2]+ttt[4]+ttt[6]))) 
+    ((target+=(board[2]+board[4]+board[6]))) 
     win_check $target
+}
+
+# AI selects choice from board
+function ai_move() {
+    choice_invalid=true
+    tries_counter=0
+    while [ $choice_invalid == true ]; do
+        ai_choice=$(shuf -i 0-8 -n 1)
+        if [[ board[ai_choice] -eq 0 ]]; then
+            choice_invalid=false
+            (( board[ai_choice]+=$1 ))
+            (( num_moves+=1 ))
+        fi
+        ((tries_counter++))
+        if [ $tries_counter -gt 30 ]; then
+            game_end 0
+        fi
+    done
+    check_board_state
 }
 
 # Run until game end
 while true; do
     print_grid
 
-    # Player interaction
-    read -rp "Select square: " selection
-    if ((selection >= 1 && selection <= 9)); then
-        if [[ ttt[$selection-1] -eq 0 ]]; then
-            (( ttt[selection-1]++ ))
-            (( num_moves+=1 ))
-        else
-            printf "Invalid input\n"
+    # Player 1 move
+    if $player_1_human; then
+        read -rp "Select square (1-9): " selection
+        if ((selection >= 1 && selection <= 9)) && \
+        [[ board[$selection-1] -eq 0 ]]; then
+                (( board[selection-1]++ ))
+                (( num_moves+=1 ))
+            else
+                printf "Invalid input\n"
         fi
+        check_board_state
     else
-        printf "Invalid input\n"
+        ai_move 1
     fi
-    check_board_state
-    
-    # AI move
-    choice_invalid=true
-    tries_counter=0
-    while [ $choice_invalid == true ]; do
-        ai_choice=$(shuf -i 0-8 -n 1)
-        if [[ ttt[ai_choice] -eq 0 ]]; then
-            choice_invalid=false
-            (( ttt[ai_choice]-=1 ))
-            (( num_moves+=1 ))
-        fi
-        ((tries_counter++))
-        if [ $tries_counter -gt 30 ]; then
-            game_end 0
-            choice_invalid=false
-        fi
-    done
-    check_board_state
+
+    # Player 2 move
+    ai_move -1
 
 done
